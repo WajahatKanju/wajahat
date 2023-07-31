94d86233b7e59cbd2983116d07bff7d025c9b9d5
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-07-31 23:09:11 +0500

    add Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
+import { BrowserRouter } from "react-router-dom"
+import {
+  Hero, Navbar, About, Tech, Experience, Works, Feedbacks, Contact, StarsCanvas
+} from './components'
 function App() {
 
   return (
-    <>
-      <h1>3D Developer Portfolio</h1>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
